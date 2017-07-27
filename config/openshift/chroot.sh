f364e5733a8a9125963c8816c6d5786d86008b6b
Scott Hebert <shebert@redhat.com>
2017-07-27 16:35:51 -0400

    add chroot test command

--- config/openshift/chroot.sh
+++ config/openshift/chroot.sh
@@ -2,3 +2,5 @@ mkdir -p /var/tmp/chroot
 yum -y --installroot=/var/tmp/chroot --releasever=7 --nogpg --disablerepo='*' --enablerepo=base install centos-release openssh-clients wget vi nano zip unzip tar mariadb findutils iputils bind-utils rsync
 echo "nameserver 8.8.8.8" >> /var/tmp/chroot/etc/resolv.conf
 echo "nameserver 8.8.4.4" >> /var/tmp/chroot/etc/resolv.conf
+#
+chroot /var/tmp/chroot "ldconfig -v"
