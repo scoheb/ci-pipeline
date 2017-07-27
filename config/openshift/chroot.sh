a8e1faf63ae0b6100b3ebc1d8f90ac355b13654b
Scott Hebert <shebert@redhat.com>
2017-07-27 16:18:18 -0400

    add -y

--- config/openshift/chroot.sh
+++ config/openshift/chroot.sh
@@ -1,4 +1,4 @@
 mkdir -p /var/tmp/chroot
-yum --installroot=/var/tmp/chroot --releasever=7 --nogpg --disablerepo='*' --enablerepo=base install centos-release openssh-clients wget vi nano zip unzip tar mariadb findutils iputils bind-utils rsync
+yum -y --installroot=/var/tmp/chroot --releasever=7 --nogpg --disablerepo='*' --enablerepo=base install centos-release openssh-clients wget vi nano zip unzip tar mariadb findutils iputils bind-utils rsync
 echo "nameserver 8.8.8.8" >> /var/tmp/chroot/etc/resolv.conf
 echo "nameserver 8.8.4.4" >> /var/tmp/chroot/etc/resolv.conf
