5b3712d46ca54a1dabf4583d77c0dd6b6424a2b0
Scott Hebert <shebert@redhat.com>
2017-11-10 10:31:25 -0500

    chgrp

--- config/Dockerfiles/fedmsg/entrypoint.sh
+++ config/Dockerfiles/fedmsg/entrypoint.sh
@@ -1,5 +1,7 @@
 ##/usr/sbin/sshd -D -e &
 IP=$(cat /etc/hosts | sed '/^$/d' | tail -1 | awk '{print $1}')
+ls -l /home
+ls -ld /home/fedmsg2
 sed -i -e "s/@@IP@@/$IP/" /home/fedmsg2/relay.py
 
 /usr/bin/fedmsg-relay --config-filename /home/fedmsg2/relay.py
