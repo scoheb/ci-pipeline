6ea1d6d877db6995df3bf2313f8903b8a5382d2b
Scott Hebert <shebert@redhat.com>
2017-11-10 09:24:30 -0500

    fix ip and remove sshd

--- config/Dockerfiles/fedmsg/entrypoint.sh
+++ config/Dockerfiles/fedmsg/entrypoint.sh
@@ -1,2 +1,5 @@
-/usr/sbin/sshd -D -e &
+##/usr/sbin/sshd -D -e &
+IP=$(cat /etc/hosts | sed '/^$/d' | tail -1 | awk '{print $1}')
+sed -i -e "s/@@IP@@/$IP/" /etc/fedmsg.d/relay.py
+
 /usr/bin/fedmsg-relay
