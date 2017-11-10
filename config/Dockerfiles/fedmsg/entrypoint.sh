30e2dc8e874ac55a07016c927883ea1a611ba909
Scott Hebert <shebert@redhat.com>
2017-11-10 09:29:51 -0500

    move config to /tmp

--- config/Dockerfiles/fedmsg/entrypoint.sh
+++ config/Dockerfiles/fedmsg/entrypoint.sh
@@ -1,5 +1,5 @@
 ##/usr/sbin/sshd -D -e &
 IP=$(cat /etc/hosts | sed '/^$/d' | tail -1 | awk '{print $1}')
-sed -i -e "s/@@IP@@/$IP/" /etc/fedmsg.d/relay.py
+sed -i -e "s/@@IP@@/$IP/" /tmp/relay.py
 
-/usr/bin/fedmsg-relay
+/usr/bin/fedmsg-relay --config-filename /tmp/relay.py
